8bf2e5377aae0f9181b9cadcc9b40c298c4fa03d
Arthorn kittinukul <joker.arthorn@gmail.com>
2019-11-13 22:49:23 +0700

    [Edited] page addfriends scan use tab to switch container

--- app/src/screens/AddFriendScreen.js
+++ app/src/screens/AddFriendScreen.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import TabNavigator from 'react-native-tab-navigator';
 import Container from '../components/Container'
+import MyQrCode from '../sections/MyQrCode'
+import CameraScanner from '../sections/CameraScanner'
 
 class AddFriendScreen extends React.Component {
   static navigationOptions = {
     title: 'Add friend',
   }
 
+  state = {
+    selectedTab: 'home',
+  }
+
   render() {
     return (
       <Container>
         <View style={{ width: '100%', height: '100%' }}>
-          <Text>Add friend</Text>
+          <TabNavigator>
+            <TabNavigator.Item
+              selected={this.state.selectedTab === 'home'}
+              title="Home"
+              renderIcon={() => <Image style={{width: 25, height: 25}} source={require('../../assets/logo.png')} />}
+              renderSelectedIcon={() => <Image style={{width: 25, height: 25}} source={require('../../assets/logo.png')} />}
+              onPress={() => this.setState({ selectedTab: 'home' })}>
+              <CameraScanner />
+            </TabNavigator.Item>
+            <TabNavigator.Item
+              selected={this.state.selectedTab === 'profile'}
+              title="Profile"
+              renderIcon={() => <Image style={{width: 25, height: 25}} source={require('../../assets/logo.png')} />}
+              renderSelectedIcon={() => <Image style={{width: 25, height: 25}} source={require('../../assets/logo.png')} />}
+              onPress={() => this.setState({ selectedTab: 'profile' })}>
+              <MyQrCode />
+            </TabNavigator.Item>
+          </TabNavigator>
         </View>
       </Container>
     )
