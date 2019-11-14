1fa7cd3aa46305afa8874c4a2ec2eeef84f1255f
Buri Paoton <Buri@igeargeek.com>
2019-11-14 18:19:36 +0700

    [Edited] register

--- app/src/screens/RegisterScreen.js
+++ app/src/screens/RegisterScreen.js
@@ -1,21 +1,87 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, TextInput, Button } from 'react-native'
 import Container from '../components/Container'
+import register from '../libs/firebase/register'
+import setDatabase from '../libs/firebase/setDatabase'
 
 class RegisterScreen extends React.Component {
   static navigationOptions = {
     header: null,
   }
 
+  state = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
+  register = () => {
+    const { name, email, password } = this.state
+
+    register(email, password)
+      .then((data) => {
+
+        setDatabase('users/'+data.user.uid, {
+          name: name,
+          email: email,
+        })
+        this.props.navigation.navigate({ 
+          routeName: 'Profile',
+          params: { uid: data.user.uid },
+      })
+      })
+      .catch(() => {
+        alert('user ไม่ถูกต้อง')
+        this.setState({ loading: false })
+      })
+  }
+
   render() {
     return (
       <Container>
-        <View style={{ width: '100%', height: '100%' }}>
-          <Text>Register</Text>
+        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={styles.text}>name</Text>
+        <TextInput
+            style={styles.input}
+            value={this.state.name}
+            onChangeText={(name) => this.setState({ name })}
+          />
+        <Text style={styles.text}>email</Text>
+        <TextInput
+            style={styles.input}
+            value={this.state.email}
+            onChangeText={(email) => this.setState({ email })}
+          />
+        <Text style={styles.text}>password</Text>
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+        />
+
+        <View>
+          <Button title="Register" onPress={this.register} />
         </View>
+         
+        </SafeAreaView>
       </Container>
     )
   }
 }
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 5,
+    width: '60%',
+    height: 40,
+    marginBottom: 10,
+  },
+  text: {
+    paddingVertical: 3,
+    fontSize: 18,
+  },
+})
+
 
 export default RegisterScreen
